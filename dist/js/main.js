49540d1cd53a4b017df1f25cf6df7359852ccefe
Michelle Lum <michellelum123@gmail.com>
2019-12-31 15:56:36 -0800

    projects page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-// To make navbar sticky, we'll add the sticky class to the navbar when we reach its scroll position, and remove the sticky class when we leave the scroll position
+// JavaScript to make navbar sticky, we'll add the sticky class to the navbar when we reach its scroll position, and remove the sticky class when we leave the scroll position
 
 // when user scrolls, execute stickyFunction
 window.onscroll = function() {
@@ -18,4 +18,27 @@ function stickyFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+// JavaScript to make slideshow buttons work to select different slides
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("technical-slide");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
